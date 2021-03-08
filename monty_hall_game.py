2abf4121d7cbeb8ea233edec5dc8839f46277e9e
UriBalsam <uribalsam@gmail.com>
2021-03-08 12:30:03 -0500

    Adding some program files

---
+++ monty_hall_game.py
@@ -0,0 +1,13 @@
+''' Pseudo code for Monty Hall game:
+    1. Launch game window, provide instructions, and show three closed doors.
+    2. The user chooses a door.
+    3. Reveal a non winning door from the remaining two doors.
+    4. Player has the chance to switch doors
+    5. If player switches, reveal new door. Otherwise reveal original door.
+    6. Record results and show the statistics for each game strategy.
+
+
+
+
+
+ '''
